0a660674110a2c18292cc531433d3c7f45787cf8
Caleb Lundquist <CalebKLundquist@gmail.com>
2018-07-17 20:55:36 +0300

    Final Product

--- src/components/changeDate.js
+++ src/components/changeDate.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ChangeDate = (title, callBack) => {
     return(
-        <button className="change-date" onClick={callBack}>
+        <button key={1} className="change-date" onClick={callBack}>
             <i class="far fa-calendar-alt"></i>
             {title}
         </button>
